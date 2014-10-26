b1cc2c0e4473ab8e579cb787e0ebd39bd32b741b
Starstrider42 <Starstrider42@users.noreply.github.com>
2014-10-26 13:51:39 -0700

    Split up code files.
    All nontrivial classes now have an eponymous file, to make finding them easier.

---
+++ src/AsteroidDataRepository.cs
@@ -0,0 +1,309 @@
+/** Stores asteroid properties for asteroids that have not been initialized as vessels
+ * @file AsteroidDataRepository.cs
+ * @author %Starstrider42
+ * @date Created October 26, 2014
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+namespace Starstrider42 {
+
+	namespace CustomAsteroids {
+
+		/** Stores data on asteroids that have not yet been loaded
+		 */
+		public class AsteroidDataRepository : ScenarioModule {
+			internal AsteroidDataRepository() {
+				loaded = false;
+				unloadedAsteroids = new SortedDictionary<Guid, ConfigNode>();
+			}
+
+			/** Wrapper function for looking up the scenario module
+			 * 
+			 * @return The AsteroidDataRepository associated with the loaded game, or null 
+			 * 	if no such module exists.
+			 * 
+			 * @exceptsafe Must not throw exceptions
+			 */
+			internal static AsteroidDataRepository findModule() {
+				try {
+					AsteroidDataRepository module = 
+						(AsteroidDataRepository) HighLogic.CurrentGame.scenarios.
+						Find(sc => sc.moduleRef is AsteroidDataRepository).moduleRef;
+					return module;
+				} catch (Exception) {
+					return null;
+				}
+			}
+
+			/** Adds data for a particular asteroid to the registry
+			 * 
+			 * @param[in] asteroid The asteroid with which the data will be permanently associated.
+			 * @param[in] data The data to store until the asteroid is loaded.
+			 * 
+			 * @pre @p asteroid has never been loaded
+			 * @pre @p asteroid contains exactly one part that has a CustomAsteroidData module
+			 * 
+			 * @exception ArgumentException Thrown if @p asteroid is already registered.
+			 * 
+			 * @exceptsafe The AsteroidDataRepository must be unchanged in the event of an exception.
+			 */
+			internal void register(Vessel asteroid, CustomAsteroidData data) {
+				var nodeForm = new ConfigNode();
+				ConfigNode.CreateConfigFromObject(data, nodeForm);
+				register(asteroid, nodeForm);
+			}
+
+			/** Adds data for a particular asteroid to the registry
+			 * 
+			 * @param[in] asteroid The asteroid with which the data will be permanently associated.
+			 * @param[in] node The data to store until the asteroid is loaded.
+			 * 
+			 * @pre @p asteroid has never been loaded
+			 * @pre @p asteroid contains exactly one part that has a CustomAsteroidData module
+			 * 
+			 * @exception ArgumentException Thrown if @p asteroid is already registered.
+			 * 
+			 * @exceptsafe The AsteroidDataRepository must be unchanged in the event of an exception.
+			 */
+			internal void register(Vessel asteroid, ConfigNode node) {
+				unloadedAsteroids.Add(asteroid.id, node);
+			}
+
+			/** Stops storing data for a particular asteroid
+			 * 
+			 * @param[in] asteroid The asteroid to remove from the registry
+			 * 
+			 * @post The registry does not contain data for @p asteroid
+			 * 
+			 * @exceptsafe The AsteroidDataRepository must be unchanged in the event of an exception.
+			 */
+			internal void unregister(Vessel asteroid) {
+				// Dictionary.Remove() does not throw in the case of a missing key
+				unloadedAsteroids.Remove(asteroid.id);
+			}
+
+			/** Returns data associated with a particular asteroid
+			 * 
+			 * @param[in] asteroid The asteroid to look up
+			 * 
+			 * @pre @p asteroid is an asteroid rather than an artificial vessel (caller's responsibility)
+			 * 
+			 * @post Returns a ConfigNode representing the CustomAsteroidData instance associated with the 
+			 * 	asteroid, or a default value if no instance is stored
+			 * 
+			 * @exception NullReferenceException Thrown if @p asteroid is null.
+			 * 
+			 * @exceptsafe The game state must be unchanged in the event of an exception.
+			 */
+			internal static ConfigNode getAsteroidData(Vessel asteroid) {
+				// Active module in vessel takes precedence
+				List<CustomAsteroidData> active = asteroid.FindPartModulesImplementing<CustomAsteroidData>();
+				if (active != null && active.Count > 0) {
+					#if DEBUG
+					Debug.Log("CustomAsteroids: CustomAsteroidData found in vessel");
+					#endif
+					var nodeForm = new ConfigNode();
+					ConfigNode.CreateConfigFromObject(active.First(), nodeForm);
+					return nodeForm;
+				}
+
+				// Unloaded but initialized asteroid?
+				foreach (ProtoPartSnapshot part in asteroid.protoVessel.protoPartSnapshots) {
+					foreach (ProtoPartModuleSnapshot module in part.modules) {
+						if (module.moduleName == "CustomAsteroidData") {
+							#if DEBUG
+							Debug.Log("CustomAsteroids: CustomAsteroidData found in protovessel");
+							#endif
+							var nodeForm = new ConfigNode();
+							module.Save(nodeForm);
+							return nodeForm;
+						}
+					}
+				}
+
+				// Uninitialized asteroid?
+				AsteroidDataRepository repo = findModule();
+				if (repo != null) {
+					try {
+						ConfigNode archiveData = repo.unloadedAsteroids[asteroid.id];
+						Debug.Log("CustomAsteroids: CustomAsteroidData found in scenario");
+						return archiveData;
+					} catch (KeyNotFoundException) {}
+				}
+
+				// When all else fails, assume default asteroid type
+				#if DEBUG
+				Debug.Log("CustomAsteroids: CustomAsteroidData not found; returning default");
+				#endif
+				var node = new ConfigNode();
+				ConfigNode.CreateConfigFromObject(new CustomAsteroidData(), node);
+				return node;
+			}
+
+			/** Called on the frame when a script is enabled just before any of the Update methods is called the first time.
+			 * 
+			 * @see[Unity Documentation] (http://docs.unity3d.com/Documentation/ScriptReference/MonoBehaviour.Start.html)
+			 * 
+			 * @todo What exceptions are thrown by StartCoroutine?
+			 */
+			public void Start()
+			{
+				GameEvents.onVesselDestroy.Add(unregister);
+				// GameEvents.onVesselGoOffRails doesn't work for some reason
+				GameEvents.onPartUnpack.Add(checkAsteroid);
+			}
+
+			/** This function is called when the object will be destroyed.
+			 * 
+			 * @see [Unity Documentation] (http://docs.unity3d.com/Documentation/ScriptReference/MonoBehaviour.OnDestroy.html)
+			 * 
+			 * @todo What exceptions are thrown by StopCoroutine?
+			 */
+			public void OnDestroy() {
+				GameEvents.onVesselDestroy.Remove(unregister);
+				GameEvents.onPartUnpack.Remove(checkAsteroid);
+			}
+
+			/** Function called when an asteroid is loaded
+			 */
+			private void checkAsteroid(Part potato) {
+				if (unloadedAsteroids.ContainsKey(potato.vessel.id) 
+						&& potato.FindModuleImplementing<CustomAsteroidData>() != null) {
+					#if DEBUG
+					Debug.Log("CustomAsteroids: Transferring registration of asteroid " + potato.vessel.vesselName);
+					#endif
+					ConfigNode newData = unloadedAsteroids[potato.vessel.id];
+					#if DEBUG
+					Debug.Log("CustomAsteroids: Desired module is " + newData);
+					#endif
+
+					List<CustomAsteroidData> oldData = potato.FindModulesImplementing<CustomAsteroidData>();
+
+					foreach (CustomAsteroidData oldModule in oldData) {
+						oldModule.Load(newData);
+					}
+					unregister(potato.vessel);
+				} // else we're good
+			}
+
+			/** Called when the module is either constructed or loaded as part of a save game
+			 * 
+			 * @param[in] node The ConfigNode representing this ScenarioModule
+			 * 
+			 * @post The module is initialized with any settings in @p node
+			 */
+			public override void OnLoad(ConfigNode node)
+			{
+				base.OnLoad(node);
+
+				#if DEBUG
+				Debug.Log("CustomAsteroids: full node = " + node);
+				#endif
+				ConfigNode thisNode = node.GetNode("KnownAsteroids");
+				if (thisNode != null) {
+					ConfigNode.LoadObjectFromConfig(this, thisNode);
+					// Have to add the dictionary by hand
+					ConfigNode dict = thisNode.GetNode("Repository");
+					if (dict != null) {
+						unloadedAsteroids.Clear();
+						foreach (ConfigNode entry in dict.GetNodes("Record")) {
+							Guid key = new Guid(entry.GetValue("Asteroid"));
+
+							unloadedAsteroids.Add(key, entry.GetNode("Data").CreateCopy());
+						}
+					}
+				}
+
+				loaded = true;
+			}
+
+			/** If true, then object is up-to-date. If false, object should be treated as uninitialized.
+			 */
+			internal bool isLoaded() {
+				return loaded;
+			}
+
+			/** Called when the save game including the module is saved
+			 * 
+			 * @param[out] node The ConfigNode representing this ScenarioModule
+			 * 
+			 * @post @p node is initialized with the persistent contents of this object
+			 */
+			public override void OnSave(ConfigNode node)
+			{
+				base.OnSave(node);
+
+				ConfigNode allData = new ConfigNode();
+				ConfigNode.CreateConfigFromObject(this, allData);
+				// Have to add the dictionary by hand
+				ConfigNode dict = new ConfigNode("Repository");
+				foreach (KeyValuePair<Guid, ConfigNode> p in unloadedAsteroids) {
+					ConfigNode entry = new ConfigNode("Record");
+					entry.AddValue("Asteroid", p.Key);
+					ConfigNode record = p.Value.CreateCopy();
+					record.name = "Data";
+					entry.AddNode(record);
+					dict.AddNode(entry);
+				}
+				allData.AddNode(dict);
+				allData.name = "KnownAsteroids";
+				node.AddNode(allData);
+
+				#if DEBUG
+				Debug.Log("CustomAsteroids: saved node = " + node);
+				#endif
+			}
+
+			/** The list of known asteroids
+			 * 
+			 * @invariant Every asteroid that has never been loaded has its Guid in @p unloadedAsteroids.
+			 * @invariant Every Guid in @p unloadedAsteroids is the pid of an unloaded asteroid.
+			 * 
+			 * @warning DO NOT store loaded asteroids in this dictionary. Their pid's may change 
+			 *  during the game, invalidating the dictionary lookup.
+			 * 	@see [Anatid's KSP API documentation] (https://github.com/Anatid/XML-Documentation-for-the-KSP-API/blob/master/src/Vessel.cs#L63) 
+			 *  for details
+			 */
+			private SortedDictionary<Guid, ConfigNode> unloadedAsteroids;
+
+			/** Flag to indicate that AsteroidDataRepository is up-to-date
+			 */
+			private bool loaded;
+		}
+
+
+		/** Workaround to let module adder be run in multiple specific scenes
+		 * 
+		 * Shamelessly stolen from Trigger Au, thanks for the idea!
+		 * 
+		 * Loaded on entering any Flight scene
+		 */
+		[KSPAddon(KSPAddon.Startup.Flight, false)]
+		internal class ADRFlight : AddScenario<AsteroidDataRepository> {
+		}
+		/** Workaround to let module adder be run in multiple specific scenes
+		 * 
+		 * Shamelessly stolen from Trigger Au, thanks for the idea!
+		 * 
+		 * Loaded on entering any SpaceCentre scene
+		 */
+		[KSPAddon(KSPAddon.Startup.SpaceCentre, false)]
+		internal class ADRSpaceCenter : AddScenario<AsteroidDataRepository> {
+		}
+		/** Workaround to let module adder be run in multiple specific scenes
+		 * 
+		 * Shamelessly stolen from Trigger Au, thanks for the idea!
+		 * 
+		 * Loaded on entering any TrackingStation scene
+		 */
+		[KSPAddon(KSPAddon.Startup.TrackingStation, false)]
+		internal class ADRTrackingStation : AddScenario<AsteroidDataRepository> {
+		}
+
+	}
+}
---
+++ src/Options.cs
@@ -0,0 +1,226 @@
+/** Saves and loads mod preferences
+ * @file Options.cs
+ * @author %Starstrider42
+ * @date Created October 26, 2014
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+namespace Starstrider42 {
+
+	namespace CustomAsteroids {
+
+		/** Stores a set of configuration options for Custom Asteroids
+		 * 
+		 * ConfigNodes are used to manage option persistence
+		 */
+		internal class Options {
+			/** Sets all options to their default values
+			 * 
+			 * @exceptsafe Does not throw exceptions.
+			 */
+			internal Options() {
+				versionNumber        = latestVersion();
+				renameAsteroids      = true;
+				minUntrackedLifetime = 1.0f;
+				maxUntrackedLifetime = 20.0f;
+				useCustomSpawner     = true;
+				errorsOnScreen       = true;
+			}
+
+			/** Stores current Custom Asteroids options in a config file
+			 * 
+			 * @post The current settings are stored to the config file
+			 * @post The current Custom Asteroids version is stored to the config file
+			 * 
+			 * @todo Identify exception conditions
+			 * 
+			 * @exceptsafe The program is in a consistent state in the event of an exception
+			 */
+			internal void Save() {
+				// File may have been loaded from a previous version
+				string trueVersion = versionNumber;
+				try {
+					versionNumber = latestVersion();
+
+					ConfigNode allData = new ConfigNode();
+					ConfigNode.CreateConfigFromObject(this, allData);		// Only overload that works!
+
+					// Create directories if necessary
+					System.IO.FileInfo outFile = new System.IO.FileInfo(optionList());
+					System.IO.Directory.CreateDirectory(outFile.DirectoryName);
+					allData.Save(outFile.FullName);
+					Debug.Log("CustomAsteroids: settings saved");
+				} finally {
+					versionNumber = trueVersion;
+				}
+			}
+
+			/** Factory method obtaining Custom Asteroids settings from a config file
+			 * 
+			 * @return A newly constructed Options object containing up-to-date 
+			 * 		settings from the Custom Asteroids config file, or the default settings 
+			 * 		if no such file exists.
+			 * 
+			 * @exception System.TypeInitializationException Thrown if the Options object 
+			 * 		could not be constructed
+			 * 
+			 * @exceptsafe The program is in a consistent state in the event of an exception
+			 * 
+			 * @todo Can I make Load() atomic?
+			 */
+			internal static Options Load() {
+				try {
+					// Start with the default options
+					Options allOptions = new Options();
+
+					// Load options
+					Debug.Log("CustomAsteroids: loading settings...");
+
+					ConfigNode optFile = ConfigNode.Load(optionList());
+					if (optFile != null) {
+						ConfigNode.LoadObjectFromConfig(allOptions, optFile);
+						// Backward-compatible with initial release
+						if (!optFile.HasValue("VersionNumber")) {
+							allOptions.versionNumber = "0.1.0";
+						}
+					} else {
+						allOptions.versionNumber = "";
+					}
+
+					if (allOptions.versionNumber != latestVersion()) {
+						// Config file is either missing or out of date, make a new one
+						// Any information loaded from previous config file will be preserved
+						try {
+							allOptions.Save();
+							if (allOptions.versionNumber.Length == 0) {
+								Debug.Log("CustomAsteroids: no config file found at " + optionList() + "; creating new one");
+							} else {
+								Debug.Log("CustomAsteroids: loaded config file from version " + allOptions.versionNumber +
+									"; updating to version " + latestVersion());
+							}
+						} catch (Exception e) {
+							// First priority, just in case Debug.Log*() produce I/O exceptions themselves
+							Debug.LogError("CustomAsteroids: settings could not be saved");
+							Debug.LogException(e);
+						}
+					}
+
+					Debug.Log("CustomAsteroids: settings loaded");
+
+					return allOptions;
+					// No idea what kinds of exceptions are thrown by ConfigNode
+				} catch (Exception e) {
+					throw new TypeInitializationException("Starstrider42.CustomAsteroids.Options", e);
+				}
+			}
+
+			/** Returns whether or not asteroids may be renamed by their population
+			 * 
+			 * @return True if renaming allowed, false otherwise.
+			 * 
+			 * @exceptsafe Does not throw exceptions
+			 */
+			internal bool getRenameOption() {
+				return renameAsteroids;
+			}
+
+			/** Returns whether or not the ARM asteroid spawner is used
+			 * 
+			 * @return True if custom spawner used, false if stock.
+			 * 
+			 * @exceptsafe Does not throw exceptions
+			 */
+			internal bool getCustomSpawner() {
+				return useCustomSpawner;
+			}
+
+			/** Returns whether or not asteroid spawning errors should appear in the game.
+			 * 
+			 * @return True if errors should be put on screen, false if logged only.
+			 * 
+			 * @exceptsafe Does not throw exceptions
+			 */
+			internal bool getErrorReporting() {
+				return errorsOnScreen;
+			}
+
+			/** Returns the time range in which untracked asteroids will disappear
+			 * 
+			 * @return The minimum (@p first) and maximum (@p second) number of days an asteroid 
+			 * 		can go untracked
+			 * 
+			 * @exception System.InvalidOperationException Thrown if @p first is negative, @p second 
+			 * 		is nonpositive, or @p first > @p second
+			 * 
+			 * @exceptsafe Program state is unchanged in the event of an exception
+			 */
+			internal Pair<float, float> getUntrackedTimes() {
+				if (minUntrackedLifetime < 0.0f) {
+					throw new InvalidOperationException("Minimum untracked time may not be negative (gave " 
+						+ minUntrackedLifetime+ ")");
+				}
+				if (maxUntrackedLifetime <= 0.0f) {
+					throw new InvalidOperationException("Maximum untracked time must be positive (gave " 
+						+ maxUntrackedLifetime+ ")");
+				}
+				if (maxUntrackedLifetime < minUntrackedLifetime) {
+					throw new InvalidOperationException("Maximum untracked time must be at least minimum time (gave " 
+						+ minUntrackedLifetime + " > " + maxUntrackedLifetime+ ")");
+				}
+				return new Pair<float, float>(minUntrackedLifetime, maxUntrackedLifetime);
+			}
+
+			/** Identifies the Custom Asteroids config file
+			 * 
+			 * @return An absolute path to the config file
+			 * 
+			 * @exceptsafe Does not throw exceptions
+			 */
+			private static string optionList() {
+				return KSPUtil.ApplicationRootPath + "GameData/CustomAsteroids/PluginData/Custom Asteroids Settings.cfg";
+			}
+
+			/** Returns the mod's current version number
+			 *
+			 * @return A version number in major.minor.patch form
+			 *
+			 * @exceptsafe Does not throw exceptions
+			 */
+			private static string latestVersion() {
+				return Assembly.GetExecutingAssembly().GetName().Version.ToString(3);
+			}
+
+			/////////////////////////////////////////////////////////
+			// Config options
+
+			/** Whether or not make to asteroid names match their population */
+			[Persistent(name="RenameAsteroids")]
+			private bool renameAsteroids;
+
+			/** Whether or not to use custom spawning behavior */
+			[Persistent(name="UseCustomSpawner")]
+			private bool useCustomSpawner;
+
+			/** Whether or not to report failed asteroid spawns in the game. The errors will be logged regardless. */
+			[Persistent(name="ErrorsOnScreen")]
+			private bool errorsOnScreen;
+
+			/** Minimum number of days an asteroid goes untracked */
+			[Persistent(name="MinUntrackedTime")]
+			private float minUntrackedLifetime;
+
+			/** Maximum number of days an asteroid goes untracked */
+			[Persistent(name="MaxUntrackedTime")]
+			private float maxUntrackedLifetime;
+
+			/** The plugin version for which the settings file was written */
+			[Persistent(name="VersionNumber")]
+			private string versionNumber;
+		}
+		
+	}
+}
