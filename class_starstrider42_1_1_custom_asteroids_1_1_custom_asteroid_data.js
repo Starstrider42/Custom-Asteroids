cd3fd1607e23419c0cf54e0b47036e75d343883e
Starstrider42 <Starstrider42@users.noreply.github.com>
2016-04-03 20:51:18 +0200

    Cleaned up CustomAsteroidData documentation.

--- class_starstrider42_1_1_custom_asteroids_1_1_custom_asteroid_data.js
+++ class_starstrider42_1_1_custom_asteroids_1_1_custom_asteroid_data.js
@@ -1,4 +1,5 @@
 var class_starstrider42_1_1_custom_asteroids_1_1_custom_asteroid_data =
 [
+    [ "CustomAsteroidData", "class_starstrider42_1_1_custom_asteroids_1_1_custom_asteroid_data.html#a283c940a9032687935567dc36a87eb2b", null ],
     [ "composition", "class_starstrider42_1_1_custom_asteroids_1_1_custom_asteroid_data.html#a66cd1e2e63282319fff20f36de9ea95b", null ]
 ];
