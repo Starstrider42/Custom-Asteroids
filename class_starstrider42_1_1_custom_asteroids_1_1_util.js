aeb9726e5faf7ebe4ad843242ee3be13c3311637
Starstrider42 <Starstrider42@users.noreply.github.com>
2017-05-29 11:20:50 -0700

    Update to version 1.5.

--- class_starstrider42_1_1_custom_asteroids_1_1_util.js
+++ class_starstrider42_1_1_custom_asteroids_1_1_util.js
@@ -1,4 +1,5 @@
 var class_starstrider42_1_1_custom_asteroids_1_1_util =
 [
-    [ "errorToPlayer", "class_starstrider42_1_1_custom_asteroids_1_1_util.html#a7cf5ca147bf695e9a3a83482630f2662", null ]
+    [ "errorToPlayer", "class_starstrider42_1_1_custom_asteroids_1_1_util.html#a7cf5ca147bf695e9a3a83482630f2662", null ],
+    [ "errorToPlayer", "class_starstrider42_1_1_custom_asteroids_1_1_util.html#a022f950415174fadb4eebe13713df954", null ]
 ];
